a7bbbf3d54f8b3cc33387c895342af5403096604
Victor LOIZEL <ext.victor.loizel@sncf.fr>
2022-07-21 08:52:01 +0200

    wip

--- src/main/java/issue/spring/graphql/objectlist/ObjectVariableArrayApplication.java
+++ src/main/java/issue/spring/graphql/objectlist/ObjectVariableArrayApplication.java
@@ -3,6 +3,9 @@ package issue.spring.graphql.objectlist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Minimal application for issue <a href="https://github.com/spring-projects/spring-graphql/issues/447">...</a>
+ */
 @SpringBootApplication
 public class ObjectVariableArrayApplication {
 
